e01e1149b66d37a3cf58d7633c66b5fea3248d9b
Alexander <tualex9029@gmail.com>
2026-09-21 12:06:05 -0500

    Limpiar JavaScript de registro

--- registration.js
+++ registration.js
@@ -8,25 +8,3 @@ function enviarExito() {
     }, 3000);
   }
 }
-
-function configurarNavegacionPaginas() {
-  const nav = document.querySelector("header nav");
-  if (!nav) return;
-
-  const enlaces = nav.querySelectorAll("a");
-
-  // Mantener el orden oficial de navegación en todas las páginas.
-  if (enlaces[0]) enlaces[0].href = "index.html";
-  if (enlaces[1]) enlaces[1].href = "eventos.html";
-  if (enlaces[2]) enlaces[2].href = "torneos.html";
-  if (enlaces[3]) enlaces[3].href = "servidores.html";
-  if (enlaces[4]) enlaces[4].href = "historial.html";
-
-  const botonHistorial = document.querySelector(".champions-panel .panel-action");
-  if (botonHistorial) botonHistorial.href = "historial.html";
-}
-
-
-document.addEventListener("DOMContentLoaded", () => {
-  configurarNavegacionPaginas();
-});
